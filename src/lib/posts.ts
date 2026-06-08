61b2742c4860f94c4350dee60bc5ccc5839aed2a
Viskeyy <50070461+Viskeyy@users.noreply.github.com>
2026-06-08 16:54:18 +0800

    docs(weekly): register programming 26-24

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -16,6 +16,7 @@ import week16 from '@/content/2026/weekly_programming_26_16.md?raw';
 import week17 from '@/content/2026/weekly_programming_26_17.md?raw';
 import week18 from '@/content/2026/weekly_programming_26_18.md?raw';
 import week21 from '@/content/2026/weekly_programming_26_21.md?raw';
+import week24 from '@/content/2026/weekly_programming_26_24.md?raw';
 
 export const postsMap: Record<string, string> = {
     weekly_programming_26_2: week2,
@@ -35,6 +36,7 @@ export const postsMap: Record<string, string> = {
     weekly_programming_26_17: week17,
     weekly_programming_26_18: week18,
     weekly_programming_26_21: week21,
+    weekly_programming_26_24: week24,
 };
 
 export const postSlugs = Object.keys(postsMap).sort((a, b) => {
